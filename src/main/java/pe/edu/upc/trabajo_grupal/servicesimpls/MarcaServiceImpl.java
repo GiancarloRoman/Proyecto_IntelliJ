ff4c94e44f568e02527bf42c687188fef2bf3793
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-10-23 01:31:37 -0500

    Update MarcaServiceImPL

--- src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/MarcaServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/MarcaServiceImpl.java
@@ -1,4 +1,40 @@
 package pe.edu.upc.trabajo_grupal.servicesimpls;
 
-public class MarcaServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+import pe.edu.upc.trabajo_grupal.repositories.IMarcaRepository;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.IMarcaService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MarcaServiceImpl implements IMarcaService {
+    @Autowired
+    private IMarcaRepository mR;
+    @Override
+    public void insert(Marca marca) {
+        mR.save(marca);
+    }
+
+    @Override
+    public List<Marca> list() {
+        return mR.findAll();
+    }
+
+    @Override
+    public void delete(int idMarca){
+        mR.deleteById(idMarca);
+    }
+
+    @Override
+    public Optional<Marca> listarId(int idMarca) {
+        return Optional.of(mR.findById(idMarca).orElse(new Marca()));
+    }
+
+    @Override
+    public List<Marca> search(String nombreMarca) {
+        return mR.search(nombreMarca);
+    }
 }
