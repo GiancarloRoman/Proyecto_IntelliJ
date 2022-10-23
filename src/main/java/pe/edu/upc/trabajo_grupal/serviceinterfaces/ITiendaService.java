dc3b7dfc436c98a963f56a00f381538a74330312
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-10-22 20:42:52 -0500

    Update ITiendaService.java

--- src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/ITiendaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/ITiendaService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.trabajo_grupal.serviceinterfaces;
 
+import pe.edu.upc.trabajo_grupal.entities.Tienda;
+import java.util.List;
+import java.util.Optional;
+
 public interface ITiendaService {
+    public void insert(Tienda tienda);
+    List<Tienda> list();
+    public void delete(int idTienda);
+    public Optional<Tienda> listarId(int idTienda);
+    List<Tienda>search(String nombreTienda);
 }
