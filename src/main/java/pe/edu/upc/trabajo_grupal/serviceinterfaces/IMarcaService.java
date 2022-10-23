ea2ba720b2c03a3ddba533b6980fa4f840f718fc
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-10-23 01:28:53 -0500

    Update IMarcaService

--- src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IMarcaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IMarcaService.java
@@ -1,4 +1,14 @@
 package pe.edu.upc.trabajo_grupal.serviceinterfaces;
 
+import pe.edu.upc.trabajo_grupal.entities.Cliente;
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+import java.util.List;
+import java.util.Optional;
+
 public interface IMarcaService {
+    public void insert(Marca marca);
+    List<Marca> list();
+    public void delete(int idMarca);
+    public Optional<Marca> listarId(int idMarca);
+    List<Marca>search(String nombreMarca);
 }
