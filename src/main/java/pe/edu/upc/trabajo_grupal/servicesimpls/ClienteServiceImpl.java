f8580a73f92f813035547377107e129ea31a0d17
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-10-22 22:25:35 -0500

    Update ClienteServiceImpl.java

--- src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/ClienteServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/ClienteServiceImpl.java
@@ -2,9 +2,9 @@ package pe.edu.upc.trabajo_grupal.servicesimpls;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pe.edu.upc.trabajo_grupal1.entities.Cliente;
-import pe.edu.upc.trabajo_grupal1.repositories.IClienteRepository;
-import pe.edu.upc.trabajo_grupal1.serviceinterfaces.IClienteService;
+import pe.edu.upc.trabajo_grupal.entities.Cliente;
+import pe.edu.upc.trabajo_grupal.repositories.IClienteRepository;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.IClienteService;
 
 import java.util.List;
 import java.util.Optional;
