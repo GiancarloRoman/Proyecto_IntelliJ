ce897ebe60ea83cada4e8c58448bf40a91d45fd1
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-10-22 22:22:53 -0500

    Update ClienteController.java

--- src/main/java/pe/edu/upc/trabajo_grupal/controllers/ClienteController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/controllers/ClienteController.java
@@ -2,8 +2,8 @@ package pe.edu.upc.trabajo_grupal.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.trabajo_grupal1.entities.Cliente;
-import pe.edu.upc.trabajo_grupal1.serviceinterfaces.IClienteService;
+import pe.edu.upc.trabajo_grupal.entities.Cliente;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.IClienteService;
 
 import java.util.List;
 @RestController
