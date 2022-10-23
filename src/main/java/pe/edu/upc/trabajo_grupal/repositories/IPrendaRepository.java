fd94b87b07e61270f39b7ea7c37570511f20f9e5
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-10-22 21:00:26 -0500

    Prenda repositories

--- src/main/java/pe/edu/upc/trabajo_grupal/repositories/IPrendaRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/repositories/IPrendaRepository.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.trabajo_grupal.repositories;
 
-public interface IPrendaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+
+import java.util.List;
+
+@Repository
+public interface IPrendaRepository extends JpaRepository<Prenda,Integer> {
+    @Query("from Prenda p where p.nombrePrenda like %:nombrePrenda")
+    List<Prenda> search(@Param("nombrePrenda") String nombrePrenda);
 }
