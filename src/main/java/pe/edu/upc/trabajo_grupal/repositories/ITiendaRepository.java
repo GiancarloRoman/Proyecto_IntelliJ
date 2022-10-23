95ec990121ce7ea336a3882b33fa92469cbace57
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-10-22 20:42:07 -0500

    Update ITiendaRepository.java

--- src/main/java/pe/edu/upc/trabajo_grupal/repositories/ITiendaRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/repositories/ITiendaRepository.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.trabajo_grupal.repositories;
 
-public interface ITiendaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.trabajo_grupal.entities.Tienda;
+
+import java.util.List;
+
+@Repository
+public interface ITiendaRepository extends JpaRepository<Tienda,Integer> {
+    @Query("from Tienda t where t.nombreTienda like %:nombreTienda")
+    List<Tienda> search(@Param("nombreTienda") String nombreTienda);
 }
