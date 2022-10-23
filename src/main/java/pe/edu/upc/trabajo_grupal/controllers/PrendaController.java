5d416c25e46bdd9b16c0b62c154b84182898671e
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-10-22 20:58:55 -0500

    Prenda cotroller

--- src/main/java/pe/edu/upc/trabajo_grupal/controllers/PrendaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/controllers/PrendaController.java
@@ -1,4 +1,39 @@
 package pe.edu.upc.trabajo_grupal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.IPrendaService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/prendas")
 public class PrendaController {
+    @Autowired
+    private IPrendaService pService;
+
+    @PostMapping
+    public void registrar(@RequestBody Prenda p) {
+        pService.insert(p);
+    }
+    @GetMapping
+    public List<Prenda> listar() {
+        return pService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        pService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Prenda prenda){
+        pService.insert(prenda);
+    }
+    @PostMapping("/buscar")
+    public List<Prenda> buscar(@RequestBody Prenda prenda){
+        List<Prenda> lista;
+        prenda.setNombrePrenda(prenda.getNombrePrenda());
+        lista=pService.search(prenda.getNombrePrenda());
+        return lista;
+    }
 }
