1929226f2dbed3322fb7303924e252de77c6023e
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-10-22 21:02:07 -0500

    Prenda serviceimpls

--- src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/PrendaServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/servicesimpls/PrendaServiceImpl.java
@@ -1,4 +1,42 @@
 package pe.edu.upc.trabajo_grupal.servicesimpls;
 
-public class PrendaServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+import pe.edu.upc.trabajo_grupal.repositories.IPrendaRepository;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.IPrendaService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PrendaServiceImpl implements IPrendaService {
+
+    @Autowired
+    private IPrendaRepository pR;
+    @Override
+    public void insert(Prenda prenda) {
+        pR.save(prenda);
+    }
+
+    @Override
+    public List<Prenda> list() {
+        return pR.findAll();
+    }
+
+    @Override
+    public void delete(int idPrenda) {
+        pR.deleteById(idPrenda);
+    }
+
+    @Override
+    public Optional<Prenda> listarId(int idPrenda) {
+        return Optional.of(pR.findById(idPrenda).orElse(new Prenda()));
+    }
+
+    @Override
+    public List<Prenda> search(String nombrePrenda) {
+        return pR.search(nombrePrenda);
+    }
 }
