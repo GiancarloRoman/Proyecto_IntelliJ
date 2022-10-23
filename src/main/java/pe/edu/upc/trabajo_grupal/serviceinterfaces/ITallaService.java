2957ae17d449bd75221049b8ea8eb314a7371023
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-10-22 23:24:44 -0500

    Update ITallaService.java

--- src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/ITallaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/ITallaService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.trabajo_grupal.serviceinterfaces;
 
+import pe.edu.upc.trabajo_grupal.entities.Talla;
+import java.util.List;
+import java.util.Optional;
+
 public interface ITallaService {
+    public void insert(Talla talla);
+    List<Talla> list();
+    public void delete(int idTalla);
+    public Optional<Talla> listarId(int idTalla);
+    List<Talla>search(String letraTalla);
 }
