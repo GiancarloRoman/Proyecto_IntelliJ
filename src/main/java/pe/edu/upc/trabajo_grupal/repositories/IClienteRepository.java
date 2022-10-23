18de31a4d0003c173b00ed9ae320d845417153ed
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-10-22 22:26:04 -0500

    Update IClienteRepository.java

--- src/main/java/pe/edu/upc/trabajo_grupal/repositories/IClienteRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/repositories/IClienteRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.trabajo_grupal.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
