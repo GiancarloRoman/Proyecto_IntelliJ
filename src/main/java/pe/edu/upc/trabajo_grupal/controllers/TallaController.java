190d7b7483e1c4121772b60e63d8287761d2f28f
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-10-22 23:21:29 -0500

    Update Talla-Controller

--- src/main/java/pe/edu/upc/trabajo_grupal/controllers/TallaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/controllers/TallaController.java
@@ -1,4 +1,40 @@
 package pe.edu.upc.trabajo_grupal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+import pe.edu.upc.trabajo_grupal.entities.Talla;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.ITallaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tallas")
 public class TallaController {
+    @Autowired
+    private ITallaService tService;
+
+    @PostMapping
+    public void registrar(@RequestBody Talla t) {
+        tService.insert(t);
+    }
+    @GetMapping
+    public List<Talla> listar() {
+        return tService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        tService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Talla talla){
+        tService.insert(talla);
+    }
+    @PostMapping("/buscar")
+    public List<Talla> buscar(@RequestBody Talla talla){
+        List<Talla> lista;
+        talla.setLetraTalla(talla.getLetraTalla());
+        lista=tService.search(talla.getLetraTalla());
+        return lista;
+    }
 }
