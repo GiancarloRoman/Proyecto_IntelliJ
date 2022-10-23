b497a9259cc3a26d4b8dab40777fb42fb82e3f07
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-10-22 21:51:51 -0500

    Update IClienteService.java

--- src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IClienteService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IClienteService.java
@@ -1,4 +1,14 @@
 package pe.edu.upc.trabajo_grupal.serviceinterfaces;
 
+import pe.edu.upc.trabajo_grupal1.entities.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IClienteService {
+    public void insert(Cliente cliente);
+    List<Cliente>list();
+    public void delete(int idPropietario);
+    public Optional<Cliente> listarId(int idCliente);
+    List<Cliente>search(String nombreCliente);
 }
