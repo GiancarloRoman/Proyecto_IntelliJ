1a8977952c04b966f43ffaffefcb205afa12c70d
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-10-23 01:27:51 -0500

    Update IMarcaRepository

--- src/main/java/pe/edu/upc/trabajo_grupal/repositories/IMarcaRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/repositories/IMarcaRepository.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.trabajo_grupal.repositories;
 
-public interface IMarcaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+
+import java.util.List;
+
+@Repository
+public interface IMarcaRepository extends JpaRepository<Marca,Integer> {
+    @Query("from Marca m where m.nombreMarca like %:nombreMarca")
+    List<Marca> search(@Param("nombreMarca") String nombreMarca);
 }
