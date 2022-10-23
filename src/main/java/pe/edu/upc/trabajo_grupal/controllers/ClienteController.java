7f3da8eee8f8b242b6c9df495e974fa60a9893a9
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-10-22 21:50:32 -0500

    Update ClienteController.java

--- src/main/java/pe/edu/upc/trabajo_grupal/controllers/ClienteController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/controllers/ClienteController.java
@@ -1,4 +1,39 @@
 package pe.edu.upc.trabajo_grupal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.trabajo_grupal1.entities.Cliente;
+import pe.edu.upc.trabajo_grupal1.serviceinterfaces.IClienteService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+    @Autowired
+    private IClienteService cService;
+
+    @PostMapping
+    public void registrar(@RequestBody Cliente c) {
+        cService.insert(c);
+    }
+    @GetMapping
+    public List<Cliente> listar() {
+        return cService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        cService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Cliente cliente){
+        cService.insert(cliente);
+    }
+    @PostMapping("/buscar")
+    public List<Cliente> buscar(@RequestBody Cliente cliente){
+        List<Cliente> lista;
+        cliente.setNombreCliente(cliente.getNombreCliente());
+        lista=cService.search(cliente.getNombreCliente());
+        return lista;
+    }
 }
+
