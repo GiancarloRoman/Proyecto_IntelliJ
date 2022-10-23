4033ac764070c15a099f8b056c8f13798b92e47c
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-10-22 21:01:19 -0500

    Prenda serviceinterfaces

--- src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IPrendaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal/serviceinterfaces/IPrendaService.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.trabajo_grupal.serviceinterfaces;
 
+import pe.edu.upc.trabajo_grupal.entities.Marca;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IPrendaService {
+    public void insert(Prenda prenda);
+    List<Prenda>list();
+    public void delete(int idPrenda);
+    public Optional<Prenda> listarId(int idPrenda);
+    List<Prenda>search(String nombrePrenda);
 }
